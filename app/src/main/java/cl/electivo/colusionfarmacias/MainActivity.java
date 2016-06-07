0bfceecfa42e40d41df0770d31e4a14cc32a06a8
Jorge Herrera <jherrerapinto@gmail.com>
2016-06-07 00:08:35 -0400

    avance segunda actividad

--- app/src/main/java/cl/electivo/colusionfarmacias/MainActivity.java
+++ app/src/main/java/cl/electivo/colusionfarmacias/MainActivity.java
@@ -2,6 +2,10 @@ package cl.electivo.colusionfarmacias;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonBuscarMedicamento = (Button) findViewById(R.id.searchMedicineButton);
+
+        buttonBuscarMedicamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ResultSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    /*public void onClickBuscar (){
+
+        Intent intent = new Intent(MainActivity.this,ResultSearchActivity.class);
+        startActivity(intent);
+    }*/
 }
